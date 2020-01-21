e0591508633df1259924df2ab831461c2131743f
Noah Parker <nparker@wpi.edu>
2020-01-21 18:23:45 -0500

    Runs whoami and outputs to screen.

--- boring.c
+++ boring.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <sys/wait.h>
 
 int main(int argc, char *argv[]) 
 {
@@ -12,12 +12,22 @@ int main(int argc, char *argv[])
 	}else if(rc == 0) 
 	{
 		//child process created.
+		
+		
 		printf("Child Process Created!\n");
+		char * args[2];
+		args[0] = "whoami";
+		args[1] = NULL;
+		execvp(args[0], args);
 	}
 	else
 	{
+		int rc_wait = wait(NULL);
 		printf("Parent Process Completed.\n");
 	}
 	
+
+
+
 	return 0;
 }
