f2970e59b298adaa210f60fabdf8e2a16b23641c
Noah Parker <nparker@wpi.edu>
2020-01-21 17:57:50 -0500

    Added some code to boring.c -- Compiles and runs.

--- boring.c
+++ boring.c
@@ -1,4 +1,23 @@
-t
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 
-
+int main(int argc, char *argv[]) 
+{
+	int rc = fork();
+	if (rc < 0) 
+	{
+		printf("fork failed!\n");
+	}else if(rc == 0) 
+	{
+		//child process created.
+		printf("Child Process Created!\n");
+	}
+	else
+	{
+		printf("Parent Process Completed.\n");
+	}
+	
+	return 0;
+}
