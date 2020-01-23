5a6039508222f3df2ae241307b4fd3f55aa451d6
Eva Bruklich <eva.bruklich@gmail.com>
2020-01-23 13:36:32 -0500

    added the thrid command section

--- boring.c
+++ boring.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 {
 	struct timeval beforeTime, afterTime;
 	gettimeofday(&beforeTime, NULL);
-	struct rusage whoamiUsage; 
+	struct rusage globalUsage; 
 	int rc = fork();
 	if (rc < 0) 
 	{
@@ -32,9 +32,9 @@ int main(int argc, char *argv[])
 		int difference = after_time_value - start_time_value;
 		difference = difference/1000;
 		
-		getrusage(RUSAGE_CHILDREN, &whoamiUsage);
-		long pgFaults = whoamiUsage.ru_majflt;
-		long unPgs = whoamiUsage.ru_minflt;
+		getrusage(RUSAGE_CHILDREN, &globalUsage);
+		long pgFaults = globalUsage.ru_majflt;
+		long unPgs = globalUsage.ru_minflt;
 		printf("\n");
 		printf("-- Statistics --\n");
 		printf("Elapsed time: %d milliseconds\n", difference);
@@ -67,9 +67,46 @@ int main(int argc, char *argv[])
 		int difference = after_time_value - start_time_value;
 		difference = difference/1000;
 		
-		getrusage(RUSAGE_CHILDREN, &whoamiUsage);
-		long pgFaults = whoamiUsage.ru_majflt;
-		long unPgs = whoamiUsage.ru_minflt;
+		getrusage(RUSAGE_CHILDREN, &globalUsage);
+		long pgFaults = globalUsage.ru_majflt;
+		long unPgs = globalUsage.ru_minflt;
+		printf("\n");
+		printf("-- Statistics --\n");
+		printf("Elapsed time: %d milliseconds\n", difference);
+		printf("Page Faults: %ld\n", pgFaults);
+		printf("Page Faults (reclaimed): %ld\n", unPgs);
+		printf("-- End of Statistics --\n");
+		printf("\n");
+		printf("Parent Process Completed.\n");
+	}
+
+	rc = fork();
+	if (rc < 0) 
+	{
+		printf("fork failed!\n");
+	}else if(rc == 0) 
+	{
+		//child process created.		
+		printf("Running command: last\n");
+		char * args[4];
+		args[0] = "ls";
+		args[1] = "-al";
+		args[2] = "/home";
+		args[3] = NULL;
+		execvp(args[0], args);
+	}
+	else
+	{
+		int rc_wait = wait(NULL);
+		gettimeofday(&afterTime, NULL);
+		int start_time_value = beforeTime.tv_usec;
+		int after_time_value = afterTime.tv_usec;
+		int difference = after_time_value - start_time_value;
+		difference = difference/1000;
+		
+		getrusage(RUSAGE_CHILDREN, &globalUsage);
+		long pgFaults = globalUsage.ru_majflt;
+		long unPgs = globalUsage.ru_minflt;
 		printf("\n");
 		printf("-- Statistics --\n");
 		printf("Elapsed time: %d milliseconds\n", difference);
