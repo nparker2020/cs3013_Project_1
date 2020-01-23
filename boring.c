acf4ee907d292a8d0660709cd3afc3212fda8b15
Noah Parker <nparker@wpi.edu>
2020-01-23 13:24:21 -0500

    last command now runs

--- boring.c
+++ boring.c
@@ -44,6 +44,41 @@ int main(int argc, char *argv[])
 		printf("\n");
 		printf("Parent Process Completed.\n");
 	}
+
+	rc = fork();
+	if (rc < 0) 
+	{
+		printf("fork failed!\n");
+	}else if(rc == 0) 
+	{
+		//child process created.		
+		printf("Running command: last\n");
+		char * args[2];
+		args[0] = "last";
+		args[1] = NULL;
+		execvp(args[0], args);
+	}
+	else
+	{
+		int rc_wait = wait(NULL);
+		gettimeofday(&afterTime, NULL);
+		int start_time_value = beforeTime.tv_usec;
+		int after_time_value = afterTime.tv_usec;
+		int difference = after_time_value - start_time_value;
+		difference = difference/1000;
+		
+		getrusage(RUSAGE_CHILDREN, &whoamiUsage);
+		long pgFaults = whoamiUsage.ru_majflt;
+		long unPgs = whoamiUsage.ru_minflt;
+		printf("\n");
+		printf("-- Statistics --\n");
+		printf("Elapsed time: %d milliseconds\n", difference);
+		printf("Page Faults: %ld\n", pgFaults);
+		printf("Page Faults (reclaimed): %ld\n", unPgs);
+		printf("-- End of Statistics --\n");
+		printf("\n");
+		printf("Parent Process Completed.\n");
+	}
 	
 	return 0;
 }
