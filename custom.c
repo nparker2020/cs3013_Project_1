1e0a13c19034c0c3261a111200870b5cb098b6ca
Noah Parker <nparker@wpi.edu>
2020-01-26 19:44:57 -0500

    Got file reading and command parsing all working.

--- custom.c
+++ custom.c
@@ -1,1 +1,48 @@
 // ebruklich & nparker
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) 
+{
+	char* fileName = "custom.txt";
+	FILE* customFile = fopen(fileName, "r");
+	
+	if(!customFile) 
+	{
+		printf("There was an error opening %s...", fileName);	
+		return -1;
+	}
+	char* line = NULL;
+	int buffer_size = 0;
+	//when line == NULL && buffer_size == 0, the getline method
+	//will automatically allocate a buffer of the correct size.
+	while( getline(&line, &buffer_size, customFile) != -1) 
+	{
+		char* splitWord = 0;
+		char* commandWord = NULL;		
+		commandWord = strtok(line, " ");
+		splitWord = commandWord;
+		char * arguments[32];
+		int count = 0;
+		while( splitWord != NULL)
+		{
+			//printf("word: %s\n", splitWord);
+			splitWord = strtok(NULL, " ");
+			if(splitWord != NULL) 
+			{
+				arguments[count] = malloc(strlen(splitWord) + 1);
+				strcpy(arguments[count], splitWord);
+				printf("argument: %s \n", arguments[count]);
+				count++;		
+			}	
+		}
+		printf("command: %s\n", commandWord);
+	}
+
+	return 0;
+}
--- multi.c
+++ multi.c
@@ -1,1 +1,29 @@
 // ebruklich & nparker
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+int main(int argc, char *argv[]) 
+{
+	char* fileName = "multi.txt";
+	FILE* customFile = fopen(fileName, "r");
+	
+	if(!customFile) 
+	{
+		printf("There was an error opening %s...", fileName);	
+		return -1;
+	}
+	char* line = NULL;
+	int buffer_size = 0;
+	//when line == NULL && buffer_size == 0, the getline method
+	//will automatically allocate a buffer of the correct size.
+	while( getline(&line, &buffer_size, customFile) != -1) 
+	{
+		printf("Read line: %s \n", line);	
+	}
+
+	return 0;
+}
